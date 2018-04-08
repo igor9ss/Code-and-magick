b4bbce9f3799939c0794a82a24e719fa11d5164a
Igor Zorin <zorin.igor.jaroslavovich@gmail.com>
2018-04-08 19:15:33 +0300

    задание на рендер похожих персонажей

---
+++ js/setup.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var MASS = [];
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARDS_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARDS_COATS_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARDS_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var getRandomNumber = function (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+var getObjects = function (MASS) {
+  for (var i = 0; i <= 3; i++) {
+    MASS[i] = {
+      name:  WIZARD_NAMES[getRandomNumber(0, WIZARD_NAMES.length)] + ' ' + WIZARDS_SURNAME[getRandomNumber(0, WIZARDS_SURNAME.length)],
+      coatColor: WIZARDS_COATS_COLORS[getRandomNumber(0,WIZARDS_COATS_COLORS.length)],
+      eyesColor: WIZARDS_EYES_COLOR[getRandomNumber(0, WIZARDS_EYES_COLOR.length)]
+    }
+  }
+  return MASS;
+};
+
+document.querySelector('.setup').classList.remove('hidden');
+
+var template = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+var fragment = document.createDocumentFragment();
+
+var renderVizard = function (wiz) {
+  var wizElement = template.cloneNode(true);
+
+  wizElement.querySelector('.setup-similar-label').textContent = wiz.name;
+  wizElement.querySelector('.wizard-coat').style.fill = wiz.coatColor;
+  wizElement.querySelector('.wizard-eyes').style.fill = wiz.eyesColor;
+
+  return wizElement;
+};
+
+for (var i = 0; i <= 3; i++) {
+  fragment.appendChild(renderVizard(getObjects(MASS)[i]));
+}
+document.querySelector('.setup-similar-list').appendChild(fragment);
+
+document.querySelector('.setup-similar').classList.remove('hidden');
